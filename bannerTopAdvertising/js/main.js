6757b02052a5e22075e3a03da1be9f1d4789dc36
leo2325 <68699115+leo2325@users.noreply.github.com>
2022-09-22 13:24:27 +0200

    Add files via upload

---
+++ bannerTopAdvertising/js/main.js
@@ -0,0 +1,7 @@
+// ENTRER CE CODE DANS LE FICHIER JS
+
+/* Disparition du bannerTop au clic */
+
+function unshowBannerTopAdvertising(){
+	document.getElementById("bannerTopAdvertising").style.animation = "myTopAdvertisingSlideOut 1s ease 0s 1 normal forwards";
+};
